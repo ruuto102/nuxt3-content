f38bba2c0c5c4ab83e6a7c3ef9e2d63991e6f704
ruuto102 <ruuto102@gmail.com>
2024-09-29 12:33:22 +0900

    Fix head's title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'Yoshi7 Blog'
+    }
+  },
+  $development: {
+    app: {
+      head: {
+        title: 'DEV'
+      }
+    }
+  },
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
 
