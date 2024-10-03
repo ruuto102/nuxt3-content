a40bd878cdde1230a68fd899681ef679531f956b
ruuto102 <ruuto102@gmail.com>
2024-10-03 18:05:23 +0900

    add HamburgerMenu

---
+++ components/content/HamburgerMenu.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <!-- ハンバーガーアイコン -->
+    <button @click="toggleMenu" class="hamburger focus:outline-none z-50 relative">
+      <span :class="{ 'transform rotate-45': isOpen }" class="block w-8 h-0.5 bg-black mb-1 transition-all duration-300"></span>
+      <span :class="{ 'opacity-0': isOpen }" class="block w-8 h-0.5 bg-black mb-1 transition-all duration-300"></span>
+      <span :class="{ '-rotate-45 -translate-y-2': isOpen }" class="block w-8 h-0.5 bg-black transition-all duration-300"></span>
+    </button>
+
+    <!-- 全画面表示のメニュー -->
+    <div v-if="isOpen" class="fixed inset-0 bg-gray-900 bg-opacity-90 flex flex-col justify-center items-center z-40">
+      <ul class="text-white text-2xl space-y-6">
+        <li><nuxt-link to="/" @click="toggleMenu">Home</nuxt-link></li>
+        <li><nuxt-link to="/about" @click="toggleMenu">About Me</nuxt-link></li>
+        <!-- <li><nuxt-link to="/contact" @click="toggleMenu">Contact</nuxt-link></li> -->
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isOpen: false, 
+    };
+  },
+  methods: {
+    toggleMenu() {
+      this.isOpen = !this.isOpen;
+    },
+  },
+};
+</script>
+
+<style scoped>
+/* ハンバーガーアイコンのスタイル */
+.hamburger {
+  position: relative;
+  z-index: 50;
+}
+
+/* メニューを開いたときのアニメーション */
+.hamburger span {
+  transition: transform 0.3s ease, opacity 0.3s ease;
+}
+</style>
---
+++ pages/about.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="bg-white shadow-lg rounded-lg p-6">
+    <h1 class="text-4xl font-semibold mb-4">About Me</h1>
+    <img src="/img/girl.png" alt="My Profile Picture" class="w-full md:w-2/5 rounded-full mx-auto" />
+    <h2 class="text-2xl font-semibold mb-2">Yoshi7</h2>
+    <p class="text-gray-600 mb-4">Web Developer from Saitama, Japan.<br>I love coding and building beautiful websites.</p>
+    
+    <h3 class="text-xl font-bold mt-4">Skills</h3>
+    <ul class="list-disc list-inside">
+      <li>HTML, CSS, JavaScript</li>
+      <li>Vue.js, Nuxt.js</li>
+      <li>Node.js</li>
+      <li>Tailwind CSS</li>
+      <li>Bootstrap</li>
+      <li>Figma</li>
+    </ul>
+
+    <h3 class="text-xl font-bold mt-4">Experience</h3>
+    <p class="text-gray-600">I have been working as a web developer and web Designer for 3 years, focusing on frontend development.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AboutPage',
+}
+</script>
+
+<style scoped>
+.container {
+  max-width: 800px;
+}
+</style>
