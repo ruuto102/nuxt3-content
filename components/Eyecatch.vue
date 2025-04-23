6688298a8ca59495b063264f96de2935322b73ea
ruuto102 <ruuto102@gmail.com>
2025-04-23 14:24:47 +0900

    デザイン修正

--- components/Eyecatch.vue
+++ components/Eyecatch.vue
@@ -1,23 +1,25 @@
 <template>
   <div class="relative h-64 md:h-96">
     <div
-      class="absolute -top-3 right-4 hidden h-64 w-64 transform rounded-full bg-yellow-50 opacity-40 transition-transform duration-700 ease-out dark:block md:right-96 md:top-1 md:h-80 md:w-80"
+      class="absolute -top-3 right-4 hidden h-64 w-64 transform rounded-full bg-yellow-50 opacity-40 transition-transform duration-700 ease-out dark:block sm:right-52 md:right-32 md:top-1 md:h-80 md:w-80 lg:right-52 xl:right-96"
       :class="{ 'scale-0': !loaded, 'scale-100': loaded }"
     />
 
     <div
-      class="absolute right-16 top-12 hidden h-48 w-48 transform rounded-full bg-yellow-100 opacity-90 transition-transform duration-700 ease-out dark:block md:right-[27rem] md:top-12 md:h-64 md:w-64"
+      class="absolute right-16 top-12 hidden h-48 w-48 transform rounded-full bg-yellow-100 opacity-90 transition-transform duration-700 ease-out dark:block sm:right-64 md:right-48 md:top-12 md:h-64 md:w-64 lg:right-64 xl:right-[27rem]"
       :class="{ 'scale-0': !loaded, 'scale-100': loaded }"
     />
 
     <p
-      class="speechBubble font-gloria absolute left-2 top-5 text-sm transition-opacity duration-1000 md:left-80 md:top-10 md:text-xl"
+      class="speechBubble font-gloria absolute left-2 top-5 text-[10px] transition-opacity duration-1000 sm:left-24 sm:text-sm md:left-24 md:top-11 md:text-lg lg:left-64 lg:top-10 xl:left-80"
       :class="{ 'opacity-0': !loaded, 'opacity-100': loaded, 'animate-seesaw': isShaking }"
     >
       I'm learning to code 🐾
     </p>
 
-    <div class="md:translate-x- absolute -right-4 top-12 md:right-96 md:top-8">
+    <div
+      class="md:translate-x- absolute -right-4 top-12 sm:right-52 md:right-24 md:top-8 lg:right-52 xl:right-96"
+    >
       <NuxtImg
         src="/img/girl.png"
         alt="My Profile Picture"
@@ -55,7 +57,6 @@ onMounted(() => {
   border-radius: 30px;
   background-color: #ffffff;
   text-align: center;
-  font-size: 16px;
   font-weight: 400;
   line-height: 1.5;
   color: #000000;
